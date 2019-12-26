d64c21ac87a8c18e7e17fc380354cca8ea7e1bc7
Fatahillah Satria Bima Seno <55565303+fathsbs@users.noreply.github.com>
2019-12-26 21:19:37 +0700

    Add files via upload

---
+++ MyShopData/src/MyShopData/MyShopData.java
@@ -0,0 +1,24 @@
+/*
+ * NAMA              : FATAHILLAH SATRIA BIMA SENO
+ * KELAS             : IF-01
+ * NIM               : 10118039
+ * DESKRIPSI PROGRAM :   
+ */
+package MyShopData;
+
+/**
+ *
+ * @author Seno
+ */
+public class MyShopData {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        loginview login = new loginview();
+        login.setVisible(true);
+    }
+    
+}
